e7896b596625a356ab9aecabbf7b709ac50e4b10
zarex1111 <lyubomirshvetsov@yandex.ru>
2024-10-10 18:43:55 +0300

    initial commit

--- src/main.java
+++ src/main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Въведете името си:");
+        String bookName = scanner.nextLine();
+        System.out.println(bookName);
     }
 }
